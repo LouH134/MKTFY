3c21204c2abf4c11847590acd50b28f5765aec36
Louis <Lharris134@gmail.com>
2021-10-20 18:21:04 -0400

    Fixed IQKeyboard problem

--- MKTFY/AppDelegate.swift
+++ MKTFY/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.shared().isEnabled = true
+        IQKeyboardManager.shared().isEnableAutoToolbar = false
+//
         return true
     }
 
